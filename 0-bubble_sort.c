3834a8cc33854e20b0e3c280eda288e122571a82
juliancastaeda <1277@holbertonschool.com>
2020-02-05 15:51:05 -0500

    upload file point 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+* bubble_sort - array and integers
+* @array: array
+* @size: size
+**/
+void bubble_sort(int *array, size_t size)
+{
+unsigned int x = 0, y = 0;
+size_t a = 0, b = 0;
+
+if (array == NULL || size <= 0)
+return;
+if (size > 1)
+{
+for (b = 0; b < size; b++)
+{
+for (a = 0; a <= (size - 2); a++)
+{
+x = array[a];
+y = array[a + 1];
+if (x > y)
+{
+array[a] = y;
+array[a + 1] = x;
+print_array(array, size);
+}
+}
+}
+}
+}
