abfb4145d8da2eb3b0420ef31ac97626a8cf9d96
juliancastaeda <1277@holbertonschool.com>
2020-02-05 17:39:49 -0500

    upload file

--- sort.h
+++ sort.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
- *
+ * 
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -15,9 +15,10 @@ const int n;
 struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
-
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif
