6b0e50632fe6037bb5145c8cf4dd1cb24aa5e165
juliancastaeda <1277@holbertonschool.com>
2020-02-06 14:00:25 -0500

    upload file

--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORTING_ALGORITHMS
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,11 +16,12 @@ const int n;
 struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
-void bubble_sort(int *array, size_t size);
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void bubble_sort(int *array, size_t size);
 
 #endif
